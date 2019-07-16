92ee545b1436621406739f5194d940d5838ac099
Štěpán Cellar <stepan.cellar@gmail.com>
2019-07-16 16:54:01 +0200

    CRUD item

--- app/controllers/ItemsController.java
+++ app/controllers/ItemsController.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import com.google.inject.Inject;
 import model.Item;
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -11,6 +14,11 @@ import views.html.items.*;
 
 public class ItemsController extends Controller {
 
+
+    @Inject
+    FormFactory formFactory;
+
+
     //for all items
     public Result index(){
         Set<Item> items=Item.allItems();
@@ -19,26 +27,60 @@ public class ItemsController extends Controller {
 
     //creating item
     public Result create(){
-        return  TODO();
+        Form<Item> itemForm = formFactory.form(Item.class);
+
+        return ok(create.render(itemForm));
     }
 
     public Result save(){
-        return  TODO();
+        Form<Item> itemForm = formFactory.form(Item.class).bindFromRequest();
+        Item item = itemForm.get();
+        Item.add(item);
+        return  redirect(routes.ItemsController.index());
     }
     public Result edit(Long id){
-        return   TODO();
+
+        Item item = Item.findById(id);
+        if(item==null){
+            return notFound("Item Not Found");
+        }
+        Form<Item> itemForm = formFactory.form(Item.class).fill(item);
+
+        return ok(edit.render(itemForm));
     }
 
     public Result update(){
-        return  TODO() ;
+        Form<Item> itemForm = formFactory.form(Item.class).bindFromRequest();
+        Item item = itemForm.get();
+        Item oldItem = Item.findById(item.getId());
+        if(oldItem == null){
+            return notFound("Item Not Found "+item.getId());
+        }
+        oldItem.setName(item.getName());
+        oldItem.setAuthor(item.getAuthor());
+        oldItem.setType(item.getType());
+        oldItem.setYear(item.getYear());
+        oldItem.setTextShort(item.getTextShort());
+        oldItem.setTextLong(item.getTextLong());
+        return redirect(routes.ItemsController.index());
     }
 
     public Result remove(Long id){
-        return  TODO();
+
+        Item item = Item.findById(id);
+        if(item==null){
+            return notFound("Item Not Found "+item.getId());
+        }
+        Item.remove(item);
+        return  redirect(routes.ItemsController.index());
     }
 
     public Result detail(Long id){
-        return  TODO();
+        Item item = Item.findById(id);
+        if(item==null){
+            return notFound("Item Not Found "+item.getId());
+        }
+        return ok(detail.render(item));
     }
 
 
